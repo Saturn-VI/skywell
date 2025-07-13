2269faae53b0fa05fabd008b94b9125655229eac
saturnvi <maxwang420@gmail.com>
2025-07-13 09:39:05 -0500

    add license + new types

--- server/main.go
+++ server/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	skywell "github.com/saturn-vi/skywell/api/skywell"
 )
 
 func main() {
 	fmt.Println("Server started!")
+	f := skywell.File{}
+	fmt.Println(f)
 }
