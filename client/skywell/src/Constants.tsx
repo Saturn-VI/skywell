f553ed0282303430de902ace0270e41e7ed2e478
saturn-vi <maxwang420@gmail.com>
2025-08-06 23:29:19 +0000

    Update everything for production and update packages

--- client/skywell/src/Constants.tsx
+++ client/skywell/src/Constants.tsx
@@ -1,7 +1,7 @@
 export const ENTRYWAY_URL = "https://bsky.social";
 
-export const SKYWELL_URL = "http://127.0.0.1:8080";
-// export const SKYWELL_URL = "https://skywell.dev";
+// export const SKYWELL_URL = "http://127.0.0.1:8080";
+export const SKYWELL_URL = "https://skywell.dev";
 
 export const SKYWELL_DID = "did:plc:tsaj4ffwyj5z6rjqaxmg5cp4";
-export const SKYWELL_SERVICE_ID = "#skywell_server"
+export const SKYWELL_SERVICE_ID = "#skywell_server";
