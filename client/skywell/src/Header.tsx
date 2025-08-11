cebc2415e240e85cc008ac5d8e67a24d6ecb8c16
saturnvi <maxwang420@gmail.com>
2025-08-11 16:31:18 -0500

    update header + import material icons package

--- client/skywell/src/Header.tsx
+++ client/skywell/src/Header.tsx
@@ -1,9 +1,26 @@
-import type { Component } from "solid-js";
+import { createEffect, createSignal, type Component } from "solid-js";
+import { isLoggedIn } from "./Auth.tsx";
 
 const Header: Component = () => {
+  const [loggedIn, setLoggedIn] = createSignal(false);
+
+  createEffect(async () => {
+    setLoggedIn(await isLoggedIn());
+  })
+
   return (
-    <div class="w-full h-16 bg-gray-900 text-white p-4 items-center flex justify-center">
+    <div class="w-full h-16 bg-gray-900 text-white p-4 items-center flex justify-between">
       <a href="/">skywell</a>
+      {loggedIn() ? (
+        <a href="/upload" class="bg-blue-500 px-4 py-2 hover:bg-blue-600">
+          Upload
+        </a>
+      ) : (
+        <a href="/login" class="bg-blue-500 px-4 py-2 hover:bg-blue-600">
+          Login
+        </a>
+      )
+      }
     </div>
   );
 };
